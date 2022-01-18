05e8f952307f9287d31d6cb830b360c9d55a8084
Grab a Treat <grabatreatapp@gmail.com>
2022-01-18 00:17:21 +0000

    Build invoices components

--- components/Invoice.tsx
+++ components/Invoice.tsx
@@ -1,5 +1,23 @@
-const Invoice = ()  => {
-    return(<li>Invoice Item</li>)
-}
+import styles from "../styles/Invoices.module.scss";
+import cn from "classnames";
+
+const Invoice = () => {
+  const status = "paid";
+  return (
+    <li className={styles.invoice}>
+      <h4 className={styles.invoiceId}>
+        <span>#</span>RT3080
+      </h4>
+      <span className={styles.name}>Jensen Huang</span>
+      <div className={styles.priceContainer}>
+        <span className={styles.date}>Due 19 Aug 2021</span>
+        <span className={styles.price}>£ 1,800.90</span>
+      </div>
+      <div className={cn(styles.status, { [styles[status]]: status })}>
+        Pending
+      </div>
+    </li>
+  );
+};
 
 export default Invoice;
--- components/InvoiceList.tsx
+++ components/InvoiceList.tsx
@@ -1,7 +1,15 @@
-const InvoiceList = () => {
-    return (
-        <ul>Invoice List</ul>
-    )
-}
+import Invoice from "./Invoice";
+
+const InvoiceList = ({ invoices }) => {
+  invoices = [0, 1];
+
+  return (
+    <ul>
+      {invoices.map((invoice) => {
+        return <Invoice invoice={invoice} />;
+      })}
+    </ul>
+  );
+};
 
 export default InvoiceList;
