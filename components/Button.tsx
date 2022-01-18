85cc64ffa7ac2f6867acfb3e406f232b1cddfceb
Grab a Treat <grabatreatapp@gmail.com>
2022-01-18 00:18:29 +0000

    Create button variations

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,14 @@
-const Button = () => {
-    return(<div>Btn</div>)
-}
+import styles from "../styles/Buttons.module.scss";
+import cn from "classnames";
+
+const Button = ({ type, title }) => {
+  return (
+    <button
+      className={cn(styles.button, { [styles[`button--${type}`]]: true })}
+    >
+      {title}
+    </button>
+  );
+};
 
 export default Button;
