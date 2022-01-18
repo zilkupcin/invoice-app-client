717f5463770d2cf7c0c153dc43bdbf13af5422fc
Grab a Treat <grabatreatapp@gmail.com>
2022-01-18 00:19:43 +0000

    Wrap with React.Fragment

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,20 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
-import HomeHeader from '../components/HomeHeader';
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.scss";
+import HomeHeader from "../components/HomeHeader";
+import InvoiceList from "../components/InvoiceList";
+import React from "react";
+import NoContent from "../components/NoContent";
 
 const Home: NextPage = () => {
   return (
-   <HomeHeader></HomeHeader>
-  )
-}
+    <React.Fragment>
+      <HomeHeader />
+      {/* <InvoiceList /> */}
+      {/* <NoContent /> */}
+    </React.Fragment>
+  );
+};
 
-export default Home
+export default Home;
