9b974b82bb7df84b6cb81a1d304f85330f09077e
Grab a Treat <grabatreatapp@gmail.com>
2022-01-18 00:16:27 +0000

    Build a Filter components

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -1,5 +1,21 @@
-const Filter = () => {
-    return(<div>Filter</div>)
-}
+import React from "react";
+import FilterItem from "./FilterItem";
+import styles from "../styles/Filter.module.scss";
+
+const Filter = ({ filters }) => {
+  return (
+    <div className={styles.filters}>
+      <div className={styles.filterHeader}>
+        Filter
+        <img src="./arrow.svg" />
+      </div>
+      <ul className={styles.filterList}>
+        {filters.map((filter) => {
+          return <FilterItem filter={filter} />;
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default Filter;
--- components/FilterItem.tsx
+++ components/FilterItem.tsx
@@ -1,5 +1,14 @@
-const FilterItem = () => {
-    return(<div>FilterItem</div>)
-}
+import styles from "../styles/Filter.module.scss";
+
+const FilterItem = ({ filter, selected }) => {
+  return (
+    <li>
+      <div className={styles.checkbox}>
+        {selected && <img src="./check.svg" />}
+      </div>
+      {filter}
+    </li>
+  );
+};
 
 export default FilterItem;
