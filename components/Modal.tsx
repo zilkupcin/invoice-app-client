fa9a59119d590c9936cb58361eaadd9ece0d58c0
Grab a Treat <grabatreatapp@gmail.com>
2022-01-18 00:15:45 +0000

    Build a Modal component

---
+++ components/Modal.tsx
@@ -0,0 +1,22 @@
+import styles from "../styles/Modal.module.scss";
+import Button from "./Button";
+
+const Modal = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.modal}>
+        <h4 className={styles.title}>Confirm deletion</h4>
+        <p className={styles.description}>
+          Are you sure you want to delete invoice #XM9141? This action cannot be
+          undone.
+        </p>
+        <div className={styles.buttons}>
+          <Button type="tertiary" title="Cancel" />
+          <Button type="secondary" title="Delete" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
