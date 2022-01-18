0dbb7ba8d4c5ef66e3bb16a57caf0ea6d756d0cf
Grab a Treat <grabatreatapp@gmail.com>
2022-01-18 00:18:56 +0000

    Increase spacing

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,19 +1,21 @@
-import styles from '../styles/Home.module.scss';
-import Filter from './Filter';
-import NewInvoiceBtn from './NewInvoiceBtn';
+import styles from "../styles/Home.module.scss";
+import Filter from "./Filter";
+import NewInvoiceBtn from "./NewInvoiceBtn";
+import filters from "../data/filters";
 
 const HomeHeader = () => {
-    return (
+  return (
     <div className={styles.homeHeader}>
-        <div className={styles.headerLeft}>
-            <h1>Invoices</h1>
-            <span>7 Invoices</span>
-        </div>
-        <div className={styles.headerRight}>
-            <Filter></Filter>
-            <NewInvoiceBtn></NewInvoiceBtn>
-        </div>
-    </div>)
-}
+      <div className={styles.headerLeft}>
+        <h1>Invoices</h1>
+        <span>7 Invoices</span>
+      </div>
+      <div className={styles.headerRight}>
+        {/* <Filter filters={filters}></Filter> */}
+        <NewInvoiceBtn></NewInvoiceBtn>
+      </div>
+    </div>
+  );
+};
 
 export default HomeHeader;
