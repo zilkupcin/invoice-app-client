323b9840b57f27b948fa75cd3b3d66dfaed4d1d2
Grab a Treat <grabatreatapp@gmail.com>
2022-01-23 20:13:34 +0000

    Create an invoice component

--- components/Invoice.tsx
+++ components/Invoice.tsx
@@ -1,21 +1,52 @@
 import styles from "../styles/Invoices.module.scss";
 import cn from "classnames";
+import { useRouter } from "next/router";
 
-const Invoice = () => {
-  const status = "paid";
+const Invoice = ({ invoice }) => {
+  const router = useRouter();
+
+  const calculatePaymentDate = () => {
+    const days = parseInt(invoice.paymentTerms.replace("net_", ""));
+    let newDate = new Date(invoice.date);
+    newDate.setDate(newDate.getDate() + days);
+
+    return newDate.toLocaleString("en-GB", {
+      timeZone: "UTC",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    });
+  };
+
+  const handleViewInvoice = () => {
+    router.push(`/invoice/${invoice._id}`);
+  };
+
+  const calculateGrandTotal = () => {
+    return invoice.items
+      .reduce((total, item) => {
+        return item.price * item.quantity + total;
+      }, 0)
+      .toFixed(2);
+  };
   return (
-    <li className={styles.invoice}>
+    <li onClick={handleViewInvoice} className={styles.invoice}>
       <h4 className={styles.invoiceId}>
         <span>#</span>RT3080
       </h4>
-      <span className={styles.name}>Jensen Huang</span>
+      <span className={styles.name}>{invoice.clientName}</span>
       <div className={styles.priceContainer}>
-        <span className={styles.date}>Due 19 Aug 2021</span>
-        <span className={styles.price}>£ 1,800.90</span>
+        <span className={styles.date}>Due {calculatePaymentDate()}</span>
+        <span className={styles.price}>£ {calculateGrandTotal()}</span>
       </div>
-      <div className={cn(styles.status, { [styles[status]]: status })}>
-        Pending
+      <div
+        className={cn(styles.status, {
+          [styles[invoice.status]]: invoice.status,
+        })}
+      >
+        {invoice.status[0].toUpperCase() + invoice.status.substring(1)}
       </div>
+      <img className={styles.arrow} src="/arrow-right.svg" />
     </li>
   );
 };
