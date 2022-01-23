83afe8de053e1dd17d76e525758ea697119461f0
Grab a Treat <grabatreatapp@gmail.com>
2022-01-23 20:12:46 +0000

    Add go back functionality

--- components/GoBack.tsx
+++ components/GoBack.tsx
@@ -1,8 +1,19 @@
+import { useRouter } from "next/router";
 import styles from "../styles/Navigation.module.scss";
 
-const GoBack = () => {
+const GoBack = ({ onBackClick }) => {
+  const router = useRouter();
+
+  const handleGoBack = () => {
+    if (onBackClick) {
+      onBackClick();
+    } else {
+      router.back();
+    }
+  };
+
   return (
-    <div className={styles.goBack}>
+    <div onClick={handleGoBack} className={styles.goBack}>
       <img src="/go-back.svg" />
       Go back
     </div>
