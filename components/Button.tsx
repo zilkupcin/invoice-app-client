c13a1de2e655b7cb4b04bf37945f98abfe583d78
Grab a Treat <grabatreatapp@gmail.com>
2022-01-23 20:10:23 +0000

    Add an onClick function as a prop

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,9 +1,10 @@
 import styles from "../styles/Buttons.module.scss";
 import cn from "classnames";
 
-const Button = ({ type, title }) => {
+const Button = ({ type, title, onClick }) => {
   return (
     <button
+      onClick={onClick}
       className={cn(styles.button, { [styles[`button--${type}`]]: true })}
     >
       {title}
