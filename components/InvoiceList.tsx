9002439a667fdbe5290065357f184f853f7fe114
Grab a Treat <grabatreatapp@gmail.com>
2022-01-23 20:14:07 +0000

    Create an invoice list component

--- components/InvoiceList.tsx
+++ components/InvoiceList.tsx
@@ -1,7 +1,15 @@
+import { useContext } from "react";
+import { LoaderContext } from "./LoaderProvider";
 import Invoice from "./Invoice";
+import { ACTION_LOAD_INVOICES } from "../data/constants";
+import Loader from "./Loader";
+import NoContent from "./NoContent";
 
 const InvoiceList = ({ invoices }) => {
-  invoices = [0, 1];
+  const { isLoading } = useContext(LoaderContext);
+
+  if (isLoading(ACTION_LOAD_INVOICES)) return <Loader />;
+  if (invoices.length === 0) return <NoContent />;
 
   return (
     <ul>
