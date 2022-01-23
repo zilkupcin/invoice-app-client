715a85d0de1ec95ed38b9e2d1927946246801c40
Grab a Treat <grabatreatapp@gmail.com>
2022-01-23 20:09:58 +0000

    Load form fields from json

---
+++ data/invoiceFormFields.js
@@ -0,0 +1,94 @@
+export const invoiceFormFields = [
+  {
+    id: "A8fHs6-mna",
+    type: "group",
+    label: "Bill From",
+    fields: [
+      {
+        id: "A3JoFBPbZO",
+        property: "streetAddress",
+        type: "text",
+        label: "Street Address",
+      },
+      {
+        id: "77a3fFbf_v",
+        property: "city",
+        type: "text",
+        label: "City",
+      },
+      {
+        id: "bint3vAhTh",
+        property: "postCode",
+        type: "text",
+        label: "Post Code",
+      },
+      {
+        id: "ciA3LZ1cLh",
+        property: "country",
+        type: "text",
+        label: "Country",
+      },
+    ],
+  },
+  {
+    id: "s78fna-nnv",
+    type: "group",
+    label: "Bill From",
+    fields: [
+      {
+        id: "Vknu0kAiXZ",
+        property: "clientName",
+        type: "text",
+        label: "Client's Name",
+      },
+      {
+        id: "M2783z7_3",
+        property: "clientEmail",
+        type: "text",
+        label: "Client's Email",
+      },
+      {
+        id: "slQ7wyAbcK",
+        property: "clientStreetAddress",
+        type: "text",
+        label: "Street Address",
+      },
+      {
+        id: "VsemhIlLiO",
+        property: "clientCity",
+        type: "text",
+        label: "City",
+      },
+      {
+        id: "Rs94H5t-tS",
+        property: "clientPostCode",
+        type: "text",
+        label: "Post Code",
+      },
+      {
+        id: "AThpvMc186",
+        property: "clientCountry",
+        type: "text",
+        label: "Country",
+      },
+      {
+        id: "lfjVpMWMPJ",
+        property: "date",
+        type: "date",
+        label: "Issue Date",
+      },
+      {
+        id: "c2GaDA7PMz",
+        property: "paymentTerms",
+        type: "select",
+        label: "Payment Terms",
+      },
+      {
+        id: "WlcRNEhSBA",
+        property: "projectDescription",
+        type: "text",
+        label: "Project Description",
+      },
+    ],
+  },
+];
