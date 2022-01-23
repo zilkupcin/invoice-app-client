7033e9232ebc2d8359fbb2f5e8b234fc0debb895
Grab a Treat <grabatreatapp@gmail.com>
2022-01-23 20:15:48 +0000

    Add dropdown options to separate file

---
+++ data/fieldOptions.js
@@ -0,0 +1,33 @@
+export const availablePaymentTerms = [
+  {
+    value: "net_1",
+    label: "Net 1 Day",
+  },
+  {
+    value: "net_7",
+    label: "Net 7 Days",
+  },
+  {
+    value: "net_14",
+    label: "Net 14 Day",
+  },
+  {
+    value: "net_30",
+    label: "Net 30 Days",
+  },
+];
+
+export const availableFilterOptions = [
+  {
+    value: "draft",
+    label: "Draft",
+  },
+  {
+    value: "pending",
+    label: "Pending",
+  },
+  {
+    value: "paid",
+    label: "Paid",
+  },
+];
