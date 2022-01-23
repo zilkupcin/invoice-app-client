363497aa7709e288d39a18e10485e03ef872ae16
Grab a Treat <grabatreatapp@gmail.com>
2022-01-23 20:16:07 +0000

    Load google fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,33 @@
-import '../styles/globals.scss'
-import Head from 'next/head'
-import type { AppProps } from 'next/app'
-import Sidebar from '../components/Sidebar';
-import styles from '../styles/Home.module.scss';
+import "../styles/globals.scss";
+import Head from "next/head";
+import type { AppProps } from "next/app";
+import Sidebar from "../components/Sidebar";
+import styles from "../styles/Home.module.scss";
+import LoaderProvider from "../components/LoaderProvider";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className={styles.page}>
       <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
-      <link href="https://fonts.googleapis.com/css2?family=Spartan:wght@500;700&display=swap" rel="stylesheet"></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Spartan:wght@500;700&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
-      <Sidebar/>
+      <Sidebar />
       <section className={styles.content}>
-        <Component {...pageProps} />
+        <LoaderProvider>
+          <Component {...pageProps} />
+        </LoaderProvider>
       </section>
     </div>
-  
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
