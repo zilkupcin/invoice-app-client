efa5ffa5e63460106b778b4d88f826b99e548b4f
Grab a Treat <grabatreatapp@gmail.com>
2022-01-27 23:53:11 +0000

    Add unique ids to field options

--- data/fieldOptions.js
+++ data/fieldOptions.js
@@ -1,17 +1,21 @@
 export const availablePaymentTerms = [
   {
+    id: "61f3290a46b4c4a28b2942b9",
     value: "net_1",
     label: "Net 1 Day",
   },
   {
+    id: "61f3291495a50c833b78c2e1",
     value: "net_7",
     label: "Net 7 Days",
   },
   {
+    id: "61f32919bdfbaad438c87bb5",
     value: "net_14",
     label: "Net 14 Day",
   },
   {
+    id: "61f3291c692a354448342131",
     value: "net_30",
     label: "Net 30 Days",
   },
@@ -19,14 +23,17 @@ export const availablePaymentTerms = [
 
 export const availableFilterOptions = [
   {
+    id: "61f32920c30b11a510242930",
     value: "draft",
     label: "Draft",
   },
   {
+    id: "61f32924c17e8639e437ea3d",
     value: "pending",
     label: "Pending",
   },
   {
+    id: "61f3292967b21512c05444cd",
     value: "paid",
     label: "Paid",
   },
