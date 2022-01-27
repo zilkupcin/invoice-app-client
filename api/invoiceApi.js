261a43d2147cd07242223f7791763c62e1478301
Grab a Treat <grabatreatapp@gmail.com>
2022-01-27 23:46:43 +0000

    Put API call functions in another file

---
+++ api/invoiceApi.js
@@ -0,0 +1,94 @@
+const endpoint = "http://localhost:5000/api/";
+
+export const getAllInvoices = async () => {
+  const headers = new Headers({
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "auth-token": localStorage.getItem("invoice_app_token"),
+  });
+
+  const response = await fetch(`${endpoint}invoices/`, {
+    headers: headers,
+  });
+  const data = await response.json();
+  return data;
+};
+
+export const addInvoice = async (invoiceData) => {
+  const headers = new Headers({
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "auth-token": localStorage.getItem("invoice_app_token"),
+  });
+
+  const response = await fetch(`${endpoint}invoices/add`, {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(invoiceData),
+  });
+
+  return response;
+};
+
+export const getInvoiceById = async (invoiceId) => {
+  const headers = new Headers({
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "auth-token": localStorage.getItem("invoice_app_token"),
+  });
+
+  const response = await fetch(`${endpoint}invoices/${invoiceId}`, {
+    headers: headers,
+  });
+
+  const data = await response.json();
+
+  return data;
+};
+
+export const deleteInvoiceById = async (invoiceId) => {
+  const headers = new Headers({
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "auth-token": localStorage.getItem("invoice_app_token"),
+  });
+
+  const response = await fetch(`${endpoint}invoices/delete/${invoiceId}`, {
+    method: "POST",
+    headers: headers,
+  });
+
+  return response;
+};
+
+export const markAsPaidById = async (data, invoiceId) => {
+  const headers = new Headers({
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "auth-token": localStorage.getItem("invoice_app_token"),
+  });
+
+  const response = await fetch(`${endpoint}invoices/edit/${invoiceId}`, {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(data),
+  });
+
+  return response;
+};
+
+export const editInvoiceById = async (data, invoiceId) => {
+  const headers = new Headers({
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "auth-token": localStorage.getItem("invoice_app_token"),
+  });
+
+  const response = await fetch(`${endpoint}invoices/edit/${invoiceId}`, {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(data),
+  });
+
+  return response;
+};
