a3324d570952fcff8c2378d4a482ca46e1c3c1b5
Grab a Treat <grabatreatapp@gmail.com>
2022-01-30 23:10:33 +0000

    Add different validation rules for draft invoices

--- validation/validation.js
+++ validation/validation.js
@@ -3,42 +3,69 @@ import Joi from "joi";
 // Invoice Validation
 export const invoiceValidation = (data) => {
   const itemSchema = Joi.object({
-    name: Joi.string().min(5).required(),
-    quantity: Joi.number().min(0).required(),
-    price: Joi.number().min(0).required(),
+    name:
+      data.status === "draft"
+        ? Joi.string().max(255).allow("")
+        : Joi.string().min(2).required(),
+    quantity:
+      data.status === "draft"
+        ? Joi.number().min(0).default(0)
+        : Joi.number().min(0).required(),
+    price:
+      data.status === "draft"
+        ? Joi.number().min(0).default(0)
+        : Joi.number().min(0).required(),
   }).unknown(true);
 
   const invoiceSchema = Joi.object({
-    streetAddress: Joi.string().min(6).required(),
-    city: Joi.string().min(2).required(),
-    postCode: Joi.string().min(2).required(),
-    country: Joi.string().min(6).required(),
-    clientName: Joi.string().min(2).required(),
+    streetAddress:
+      data.status === "draft"
+        ? Joi.string().max(255).allow("")
+        : Joi.string().min(6).required(),
+    city:
+      data.status === "draft"
+        ? Joi.string().max(255).allow("")
+        : Joi.string().min(2).required(),
+    postCode:
+      data.status === "draft"
+        ? Joi.string().max(255).allow("")
+        : Joi.string().min(2).required(),
+    country:
+      data.status === "draft"
+        ? Joi.string().max(255).allow("")
+        : Joi.string().min(2).required(),
+    clientName:
+      data.status === "draft"
+        ? Joi.string().max(255).allow("")
+        : Joi.string().min(2).required(),
     clientEmail: Joi.string()
       .email({ tlds: { allow: false } })
       .required(),
-    clientStreetAddress: Joi.string().min(6).required(),
-    clientCity: Joi.string().min(2).required(),
-    clientPostCode: Joi.string().min(2).required(),
-    clientCountry: Joi.string().min(6).required(),
-    date: Joi.date().required(),
-    paymentTerms: Joi.string()
-      .valid("net_1", "net_7", "net_14", "net_30")
-      .required(),
-    projectDescription: Joi.string().min(6).required(),
+    clientStreetAddress:
+      data.status === "draft"
+        ? Joi.string().max(255).allow("")
+        : Joi.string().min(2).required(),
+    clientCity:
+      data.status === "draft"
+        ? Joi.string().max(255).allow("")
+        : Joi.string().min(2).required(),
+    clientPostCode:
+      data.status === "draft"
+        ? Joi.string().max(255).allow("")
+        : Joi.string().min(2).required(),
+    clientCountry:
+      data.status === "draft"
+        ? Joi.string().max(255).allow("")
+        : Joi.string().min(2).required(),
+    date: Joi.date(),
+    paymentTerms: Joi.string().valid("net_1", "net_7", "net_14", "net_30"),
+    projectDescription:
+      data.status === "draft"
+        ? Joi.string().max(255).allow("")
+        : Joi.string().min(2).required(),
     status: Joi.string().valid("draft", "pending", "paid").required(),
     items: Joi.array().min(1).items(itemSchema),
   }).unknown(true);
 
   return invoiceSchema.validate(data, { abortEarly: false });
-};
-
-export const itemValidation = (data) => {
-  const schema = Joi.object({
-    name: Joi.string().min(5).required(),
-    quantity: Joi.number().min(0).required(),
-    price: Joi.number().min(0).required(),
-  }).unknown(true);
-
-  return schema.validate(data);
 };
