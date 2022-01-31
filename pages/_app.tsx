1873087e0d14e4576fe1c6924aa0b66eea1a3a20
Grab a Treat <grabatreatapp@gmail.com>
2022-01-31 08:37:51 +0000

    Change title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,6 +22,8 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <link rel="shortcut icon" href="/images/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <title>Invoice App</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
